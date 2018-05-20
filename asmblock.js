61c8ac3331a231daa965a8897e93bf192d74a857
RetroAsgardian <oskarsmacaw@gmail.com>
2018-05-20 16:22:26 -0400

    Initial commit

---
+++ asmblock.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.tooltipped').tooltip({enterDelay: 0, exitDelay: 0, inDuration: 150, outDuration: 100, transitionMovement: 5});
+	$('.collapsible').collapsible({});
+	$('.collapsible-header').addClass('waves-effect waves-block');
+	$('.modal').modal({});
+	$('.scrollspy').scrollSpy({});
+	var elems = document.querySelectorAll('.tap-target');
+	var instances = M.TapTarget.init(elems, {});
+	$.each(instances, function(index, instance) {
+		instance.open();
+	});
+	setTimeout(function() {
+		$.each(instances, function(index, instance) {
+			instance.close();
+		});
+	}, 3000);
+});
